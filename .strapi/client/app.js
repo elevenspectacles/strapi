d836becd1d74b095a23a725d74bb02cb8c6b2899
yordan.ramchev <yordan.ramchev@mentormate.com>
2024-01-24 16:19:02 +0200

    Initial project setup

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -7,7 +7,6 @@ import i18N from "@strapi/plugin-i18n/strapi-admin";
 import seo from "@strapi/plugin-seo/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
 import generateData from "strapi-plugin-generate-data/strapi-admin";
-import strapiStripe from "strapi-stripe/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
 renderAdmin(document.getElementById("strapi"), {
@@ -17,6 +16,5 @@ renderAdmin(document.getElementById("strapi"), {
     seo: seo,
     "users-permissions": usersPermissions,
     "generate-data": generateData,
-    "strapi-stripe": strapiStripe,
   },
 });
