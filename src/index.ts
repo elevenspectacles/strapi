cb0a684f3403a5fa805216778b9d1bc3168b1d69
yordan.ramchev <yordan.ramchev@mentormate.com>
2024-01-24 20:39:21 +0200

    Initial project setup

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-'use strict';
+import { Strapi } from "@strapi/strapi";
 
-module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
-
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
+export default {
+  register({ strapi }: { strapi: Strapi }) {},
 };
