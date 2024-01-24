f36cdf41701d25f42ac142fdea0f948a9aff7067
yordan.ramchev <yordan.ramchev@mentormate.com>
2024-01-24 23:25:16 +0200

    Initial project setup

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,21 +1,21 @@
-module.exports = [
-  'strapi::errors',
+export default [
+  "strapi::errors",
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         directives: {
-          frameAncestors: ['http://localhost:*', 'self']
-        }
-      }
+          frameAncestors: ["http://localhost:*", "self"],
+        },
+      },
     },
   },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
