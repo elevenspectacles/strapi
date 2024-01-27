630e7e58e95ac8fc8881480f9519a2880f19d788
yordan.ramchev <yordan.ramchev@mentormate.com>
2024-01-28 00:03:35 +0200

    Initial project setup

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,10 +1,10 @@
 export default ({ env }) => ({
   proxy: true,
-  url: env("APP_URL", "https://elevenspectacles.com/api"), // Sets the public URL of the application.
+  url: `${env("APP_URL")}/api`, // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
   },
   admin: {
-    url: "/api/admin",
+    url: "/admin",
   },
 });
